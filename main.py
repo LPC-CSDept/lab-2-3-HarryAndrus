288f12099a31e6fcceb922772d36198bbb22f18a
HarryAndrus <iamjoshmay@gmail.com>
2023-02-02 10:37:28 -0800

    Lab3

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-
-    pass
+    val1 = int(input('What is the First integer? '))
+    val2 = int(input('What is the Second integer? '))
+    val3 = int(input('What is the Third integer? '))
+    sum = (val1+val2+val3)
+    avg = (sum / 3)
+    print('Values:',val1,val2, val3)
+    print(f'Total: \t {sum:.2f}')
+    print(f'Average: {avg:.2f}')
 
 
 if __name__ == '__main__':
